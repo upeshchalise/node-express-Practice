08c02db1bed74947e03ae582182f09460031dcb0
upeshchalise <upeshchalise@gmail.com>
2023-09-19 22:00:38 +0545

    start

--- projects/contact_management/server.js
+++ projects/contact_management/server.js
@@ -1,3 +1,8 @@
-import express from "express";
-
+const express = require("express");
 const app = express();
+
+const port = 5000;
+
+app.listen(port, () => {
+  console.log(`server is listening at the port ${port}`);
+});
