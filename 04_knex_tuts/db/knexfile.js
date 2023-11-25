f1342c8a0bcc50ce9a053935cf8f30de610c53c8
upeshchalise <upeshchalise@gmail.com>
2023-11-25 12:18:32 +0545

    env package added

--- 04_knex_tuts/db/knexfile.js
+++ 04_knex_tuts/db/knexfile.js
@@ -1,12 +1,13 @@
 // Update with your config settings.
+require("dotenv").config();
 
 module.exports = {
   development: {
-    client: "postgresql",
+    client: process.env.DB_CLIENT,
     connection: {
-      database: "",
-      user: "",
-      password: "",
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
     },
     pool: {
       min: 2,
--- 04_knex_tuts/index.js
+++ 04_knex_tuts/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-// require("dotenv").config;
+require("dotenv").config;
 const router = require("./db/routes/router.modal");
 const port = 3000;
 app.use(express.urlencoded({ extended: false }));
