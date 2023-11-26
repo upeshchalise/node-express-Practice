2be03cb31edd67afc00cf7c1f2adef6ebef37cba
upeshchalise <upeshchalise@gmail.com>
2023-11-26 16:27:18 +0545

    methods

--- 02_express/final/09-methods.js
+++ 02_express/final/09-methods.js
@@ -11,6 +11,14 @@ app.use(express.urlencoded({ extended: false }));
 //parse json
 app.use(express.json());
 
+app.post("/login", (req, res) => {
+  const { name } = req.body;
+  if (name) {
+    return res.status(200).send(`Welcome ${name}`);
+  }
+  res.status(401).send(`Please provide credentials`);
+});
+
 app.get("/api/people", (req, res) => {
   res.status(200).json({ success: true, data: people });
 });
@@ -59,14 +67,6 @@ app.delete("/api/people/:id", (req, res) => {
 
   const newPeople = people.filter((person) => person.id !== Number(id));
   return res.status(200).json({ success: true, data: newPeople });
-});
-
-app.post("/login", (req, res) => {
-  const { name } = req.body;
-  if (name) {
-    return res.status(200).send(`Welcome ${name}`);
-  }
-  res.status(401).send(`Please provide credentials`);
 });
 
 app.listen(3000, () => {
